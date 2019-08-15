dfaed23b4b27141e6bd8b33e52ded88197f374fd
Jean Chassoul <jchassoul@gmail.com>
2019-08-15 16:50:22 -0700

    clean, clean, clean

---
+++ src/tactics.lua
@@ -0,0 +1,17 @@
+-- 3 Hatch Zergling
+-- 3 Hatch Hydralisk
+-- 3 Hatch Spire
+-- 3 Hatch Lurkers
+-- 4 Hatch before gas 
+-- 3 Base Spire into 5 Hatch Hydra
+-- 5 Pool
+-- 6 Hatch (vs. Protoss)
+
+-- 13 Pool Muta
+-- 2 Hatch Muta
+-- 3 Hatch Muta
+-- 3 Hatch Lurkers
+-- 3 Hatch Before Pool
+-- 3 Hatch Zergling
+-- 5 Pool
+-- Fake 3 Hatch Muta (vs. Terran)
