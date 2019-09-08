f22e7b681c69f92d8295c64a76e551c6dae7c133
Jean Chassoul <jchassoul@gmail.com>
2019-09-08 16:51:01 -0700

    clean, clean, clean

--- src/economy.lua
+++ src/economy.lua
@@ -171,6 +171,11 @@ function economy.manage_economy(actions, tc)
 
             if pos ~= nil then 
                 print('starting location: x '..pos[1] .. ' y ' .. pos[2])
+                if pos[1] > 256 and pos[2] <= 256 then print('quadrant A')
+                elseif pos[1] <= 256 and pos[2] <= 256 then print('quadran B')
+                elseif pos[1] <= 256 and pos[2] > 256 then print('quadran C')
+                elseif pos[1] > 256 and pos[2] >= 256 then print('quadran D')
+                else print('something is very, very wrong!') end
             end
 
         elseif ut.type == tc.unittypes.Zerg_Zergling then
