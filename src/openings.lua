f5c77cb478d87032c065406e8fd911d08d43fca8
Jean Chassoul <jchassoul@gmail.com>
2019-09-22 16:24:00 -0700

    clean, clean, clean

--- src/openings.lua
+++ src/openings.lua
@@ -1,19 +1,26 @@
 --
 -- 7 Pool
 -- 9 Pool
--- 12 Pool
 -- Overpool
 -- 12 Hatch
 --
 
 local openings = {}
 
+function openings.sevenpool()
+    -- all-in with backup
+end
+
+function openings.ninepool()
+    -- Standard ZvZ
+end
+
 function openings.overpool()
-    --
+    -- 9=
 end
 
 function openings.twelve_hatch()
-    --
+    -- (=
 end
 
 return openings
