94c5100de3c08e340325c758eb31805cc3bd9d86
Jean Chassoul <jchassoul@gmail.com>
2019-09-28 17:49:55 -0700

    clean, clean, clean

--- src/main.lua
+++ src/main.lua
@@ -9,7 +9,7 @@ local argparse = require("argparse")
 local socket = require("socket")
 local uuid = require("uuid")
 local fun = require("moses")
-local tc = require("torchcraft")
+local lc = require("lovecraft")
 local counters = require("ophelia.counters")
 local economy = require("ophelia.economy")
 local openings = require("ophelia.openings")
@@ -20,7 +20,7 @@ local zstreams = require("ophelia.zstreams")
 -- Set default float tensor type
 torch.setdefaulttensortype('torch.FloatTensor')
 -- Debug can take values 0, 1, 2 (from no output to most verbose)
-tc.DEBUG = 0 
+lc.DEBUG = 0 
 -- Set random seed
 uuid.randomseed(socket.gettime()*10000)
 -- Spawn session id
@@ -43,18 +43,18 @@ local skip_frames = 7
 local restarts = -1
 while restarts < 0 do
     restarts = restarts + 1
-    tc:init(hostname, port)
+    lc:init(hostname, port)
     local loops = 1
-    local update = tc:connect(port)
-    if tc.DEBUG > 1 then
+    local update = lc:connect(port)
+    if lc.DEBUG > 1 then
         print('Received init: ', update)
     end
-    assert(tc.state.replay == false)
+    assert(lc.state.replay == false)
     local setup = {
-        tc.command(tc.set_speed, 0), tc.command(tc.set_gui, 1),
-        tc.command(tc.set_cmd_optim, 1),
+        lc.command(lc.set_speed, 0), lc.command(lc.set_gui, 1),
+        lc.command(lc.set_cmd_optim, 1),
     }
-    tc:send({table.concat(setup, ':')})
+    lc:send({table.concat(setup, ':')})
 
     local ophelia = {}
     local units = {}
@@ -63,18 +63,18 @@ while restarts < 0 do
     -- Measure execution time
     local tm = torch.Timer()
 
-    while not tc.state.game_ended do
+    while not lc.state.game_ended do
         local actions = {}
         tm:reset()
         -- receive update from game engine
-        update = tc:receive()
+        update = lc:receive()
 
         -- How enable debug in a way that fit this "crafting" style?
-        if tc.DEBUG > 1 then
+        if lc.DEBUG > 1 then
             print('Received update: ', update)
         end
         -- O=
-        for k, v in pairs(tc.state.player_info) do
+        for k, v in pairs(lc.state.player_info) do
             if v['name'] == "Ophelia" then
                 ophelia = v
             end
@@ -82,13 +82,13 @@ while restarts < 0 do
 
         -- !!
 
-        --print(tc.state.map_name)
+        --print(lc.state.map_name)
         
-        --print(tc.state.ground_height_data)
+        --print(lc.state.ground_height_data)
 
-        --print(tc.state.buildable_data)
+        --print(lc.state.buildable_data)
         
-        --for k,v in pairs(tc.state.units_myself) do
+        --for k,v in pairs(lc.state.units_myself) do
         --    for f,e in pairs(v) do
         --        --print(v[f])
         --        print(v['hp'])
@@ -98,9 +98,9 @@ while restarts < 0 do
         --    end
         --end
 
-        --for k,v in pairs(tc.state.start_locations) do
+        --for k,v in pairs(lc.state.start_locations) do
         --    print(k)
-        --    for a,b in pairs(tc.state.start_locations[k]) do
+        --    for a,b in pairs(lc.state.start_locations[k]) do
         --        print(a)
         --        print(b)
         --    end
@@ -110,50 +110,50 @@ while restarts < 0 do
 
         -- !?
         
-        --print(inspect(getmetatable(tc.state.frame)))
+        --print(inspect(getmetatable(lc.state.frame)))
 
-        --print(inspect(tc.state.frame["toTable"](tc.state.frame)))
+        --print(inspect(lc.state.frame["toTable"](lc.state.frame)))
 
-        --units = tc.state.frame["getUnits"](tc.state.frame, ophelia['id'])
-        --resources = tc.state.frame["getResources"](tc.state.frame, ophelia['id'])
+        --units = lc.state.frame["getUnits"](lc.state.frame, ophelia['id'])
+        --resources = lc.state.frame["getResources"](lc.state.frame, ophelia['id'])
         
         --print(inspect(units))
 
         -- ?!
         
         loops = loops + 1
-        if tc.state.battle_frame_count % skip_frames == 0 then
+        if lc.state.battle_frame_count % skip_frames == 0 then
 
             -- here is exactly where actions start to execute
             -- 9734 is not ZvP standard play but this is just a 9734 hack for now. (=
-            actions = economy.manage_9734_economy(actions, tc)
+            actions = economy.manage_9734_economy(actions, lc)
            
             -- sometimes the first overlord defines our opening!
-            actions = scouting.first_overlord(actions, tc)
+            actions = scouting.first_overlord(actions, lc)
             
             -- can't do much if don't know what you are against
-            enemy = scouting.identify_enemy_units(tc.state.units_enemy, tc)
+            enemy = scouting.identify_enemy_units(lc.state.units_enemy, lc)
 
             if scouting.identify_enemy_race() then
                 print("Ophelia vs " .. scouting.identify_enemy_race())
             end
 
-        elseif tc.state.game_ended then
+        elseif lc.state.game_ended then
             break
         else
             -- skip frame do nothing
         end
         
         -- if debug make some noise!
-        if tc.DEBUG > 1 then
-            print('Frame ' .. tc.state.battle_frame_count  
+        if lc.DEBUG > 1 then
+            print('Frame ' .. lc.state.battle_frame_count  
             .. ' consume ' .. tm:time().real .. ' seconds')
             print("Sending actions: " .. actions)
         end
         
-        tc:send({table.concat(actions, ':')})
+        lc:send({table.concat(actions, ':')})
     end
-    tc:close()
+    lc:close()
     collectgarbage()
     sys.sleep(0.0042)
     print("So Long, and Thanks for All the Fish!")
