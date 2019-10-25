6d55bb71f0277ca71de88a4485817f5875dc83a6
Jean Chassoul <jchassoul@gmail.com>
2019-10-25 10:49:58 -0700

    clean, clean, clean

--- src/counters.lua
+++ src/counters.lua
@@ -1,11 +1,11 @@
 -- 2 Hatch Muta (vs. Protoss)
 -- 3 Hatch Speedlings
 -- 3 BS into 5 HH
--- 9734                          <--   YOU ARE HERE 
+-- 973(4)
 
 -- 2 Hatch Muta (vs. Terran)
 -- 3 Hatch Speedlings 
--- 3 Hatch Muta
+-- 3 Hatch Muta                  <---    YOU ARE HERE!
 
 -- Mutaling (vs. Zerg)
 
